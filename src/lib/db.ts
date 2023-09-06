984649cec88ad4f5d74c08404482b66b970085d5
eddysims <eddysimsweb@gmail.com>
2023-09-06 11:11:51 -0600

    add subdomains

---
+++ src/lib/db.ts
@@ -0,0 +1,74 @@
+// Dummy data to be replaced with your database
+const hostnamesDB = [
+  {
+    name: 'Example Site 1',
+    description: 'Subdomain + custom domain',
+    subdomain: 'test1',
+    customDomain: 'custom-domain-1.com',
+    defaultForPreview: true,
+  },
+  {
+    name: 'Example Site 2',
+    description: 'Subdomain only',
+    subdomain: 'test2',
+  },
+  {
+    name: 'Example Site 3',
+    description: 'Subdomain only',
+    subdomain: 'test3',
+  },
+]
+
+const DEFAULT_HOST = hostnamesDB.find((h) => h.defaultForPreview)
+
+/**
+ * Returns the data of the hostname based on its subdomain or custom domain
+ * or the default host if there's no match.
+ *
+ * This method is used by middleware.ts
+ */
+export async function getHostnameDataOrDefault(
+  subdomainOrCustomDomain?: string
+) {
+  if (!subdomainOrCustomDomain) return DEFAULT_HOST
+
+  // check if site is a custom domain or a subdomain
+  const customDomain = subdomainOrCustomDomain.includes('.')
+
+  // fetch data from mock database using the site value as the key
+  return (
+    hostnamesDB.find((item) =>
+      customDomain
+        ? item.customDomain === subdomainOrCustomDomain
+        : item.subdomain === subdomainOrCustomDomain
+    ) ?? DEFAULT_HOST
+  )
+}
+
+/**
+ * Returns the data of the hostname based on its subdomain.
+ *
+ * This method is used by pages under middleware.ts
+ */
+export async function getHostnameDataBySubdomain(subdomain: string) {
+  return hostnamesDB.find((item) => item.subdomain === subdomain)
+}
+
+/**
+ * Returns the paths for `getStaticPaths` based on the subdomain of every
+ * available hostname.
+ */
+export async function getSubdomainPaths() {
+  // get all sites that have subdomains set up
+  const subdomains = hostnamesDB.filter((item) => item.subdomain)
+
+  // build paths for each of the sites in the previous two lists
+  console.log({subdomains})
+  return subdomains.map((item) => {
+    console.log(item.subdomain)
+    const { subdomain} = item
+    return { params: { site: `${subdomain}` } }
+  })
+}
+
+export default hostnamesDB
---
+++ src/pages/_sites/[site]/index.tsx
@@ -0,0 +1,53 @@
+import { useRouter } from "next/router";
+// we will create these in the next step
+import { getHostnameDataBySubdomain, getSubdomainPaths } from "@/lib/db";
+
+// Our types for the site data
+export interface Props {
+  name: String
+  description: String
+  subdomain: String
+  customDomain: String
+}
+
+export default function Index(props: Props) {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return (
+      <>
+        <p>
+          Loading...
+        </p>
+      </>
+    )
+  }
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-primary">
+      <h1 className="text-xl">
+        {props.name}
+      </h1>
+    </div>
+  )
+}
+
+// Getting the paths for all the subdomains in our database
+export async function getStaticPaths() {
+  const paths = await getSubdomainPaths()
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+// Getting data to display on each custom subdomain
+export async function getStaticProps({ params: { site } }) {
+  const sites = await getHostnameDataBySubdomain(site)
+
+  return {
+    props: sites,
+    revalidate: 3600
+  }
+}
